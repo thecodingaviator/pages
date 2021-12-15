de3883793239c3adc37ed5b0c456b7010844fca4
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2021-12-15 17:32:10 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://workers.hiparthparth7631.workers.dev/posts", {
+      method: 'GET'
+    })
+      .then(res => {console.clear(); console.log(res.json()); return res.json()})
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setPosts(result);
+        },
+
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <ul>
+          {posts.map(post => (
+            <li>
+              {post.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
